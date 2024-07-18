92b92bbaaf10eb16663303c50e22c01afbb58b6f
Mike-0000 <93947725+Mike-0000@users.noreply.github.com>
2024-07-17 18:00:21 -0600

    Revert "Update index.js"
    This reverts commit 4273865ac04f0cd833e5bce5b7b62b4b7186d005.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const path = require('path');
+const http = require('http');
 
-const app = express();
+const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
-// Serve static files from the root folder
-app.use(express.static(path.join(__dirname)));
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
 
-app.listen(port, () => {
-  console.log(`Server running at http://0.0.0.0:${port}/`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
